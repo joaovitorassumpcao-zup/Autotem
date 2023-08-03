ddbe312e26a2e65b175e372c0487053db53e2242
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-03 19:09:06 -0300

    Misc

--- src/main/java/com/fastfood/autotem/dto/ProdutoDTO.java
+++ src/main/java/com/fastfood/autotem/dto/ProdutoDTO.java
@@ -7,9 +7,9 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 
-public record ProdutoDTO(Long id, String nome, BigDecimal preco, TipoProduto tipoProduto)
-        implements Serializable
-{
+public record ProdutoDTO(
+        Long id, String nome, BigDecimal preco, TipoProduto tipoProduto
+) implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 }
