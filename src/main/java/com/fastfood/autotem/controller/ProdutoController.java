5323efcd9de5fe87aed3df1064d98e744e029e24
João Vitor Cunha Assumpção <joao.assumpcao@zup.com.br>
2023-08-03 21:35:45 -0300

    ProdutoController

--- src/main/java/com/fastfood/autotem/controller/ProdutoController.java
+++ src/main/java/com/fastfood/autotem/controller/ProdutoController.java
@@ -9,12 +9,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
-
     private final ProdutoService produtoService;
 
     @Autowired
@@ -31,8 +31,15 @@ public class ProdutoController {
         return ResponseEntity.ok(dtos);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> getProdutoById(@PathVariable Long id) {
+        Optional<Produto> produto = produtoService.getProdutoById(id);
+        return produto.map(p -> ResponseEntity.ok(new ProdutoDTO(p.getId(), p.getNome(), p.getPreco(), p.getTipoProduto())))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
     @PostMapping
-    public ResponseEntity<ProdutoDTO> addProduto(@RequestBody ProdutoDTO produtoDTO) {
+    public ResponseEntity<ProdutoDTO> createProduto(@RequestBody ProdutoDTO produtoDTO) {
         Produto produto = new Produto();
         produto.setNome(produtoDTO.nome());
         produto.setPreco(produtoDTO.preco());
@@ -40,5 +47,27 @@ public class ProdutoController {
         Produto savedProduto = produtoService.createProduto(produto);
         ProdutoDTO savedDto = new ProdutoDTO(savedProduto.getId(), savedProduto.getNome(), savedProduto.getPreco(), savedProduto.getTipoProduto());
         return ResponseEntity.status(HttpStatus.CREATED).body(savedDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> updateProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoDTO) {
+        Optional<Produto> existingProduto = produtoService.getProdutoById(id);
+        if (existingProduto.isPresent()) {
+            Produto produto = existingProduto.get();
+            produto.setNome(produtoDTO.nome());
+            produto.setPreco(produtoDTO.preco());
+            produto.setTipoProduto(produtoDTO.tipoProduto());
+            Produto updatedProduto = produtoService.updateProduto(produto);
+            ProdutoDTO updatedDto = new ProdutoDTO(updatedProduto.getId(), updatedProduto.getNome(), updatedProduto.getPreco(), updatedProduto.getTipoProduto());
+            return ResponseEntity.ok(updatedDto);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProdutoById(@PathVariable Long id) {
+        produtoService.deleteProdutoById(id);
+        return ResponseEntity.noContent().build();
     }
 }
